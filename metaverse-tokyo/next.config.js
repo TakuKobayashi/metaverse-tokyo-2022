1e1af27692784dbc76599ab3de53f3f35273c5ab
TakuKobayashi <keep_slimbody@yahoo.co.jp>
2022-09-17 17:11:44 +0900

    insert dotenv and mapbox token.

--- metaverse-tokyo/next.config.js
+++ metaverse-tokyo/next.config.js
@@ -1,7 +1,10 @@
+const configedEnv = require('dotenv').config();
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  env: configedEnv.parsed,
 }
 
 module.exports = nextConfig
