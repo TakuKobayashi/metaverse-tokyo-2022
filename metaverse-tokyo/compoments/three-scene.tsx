32783037a9799b412e1affe635bfa5a46a587dde
TakuKobayashi <keep_slimbody@yahoo.co.jp>
2022-09-17 20:19:43 +0900

    TPSのようにカメラが追随するような感じにしてみた

--- metaverse-tokyo/compoments/three-scene.tsx
+++ metaverse-tokyo/compoments/three-scene.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { WebGLRenderer, Scene, PerspectiveCamera, DirectionalLight, Color } from 'three';
+import { WebGLRenderer, Scene, PerspectiveCamera, DirectionalLight, Color, Group } from 'three';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
 import { VRM } from '@pixiv/three-vrm';
 import { OrbitControls } from 'three-orbitcontrols-ts';
@@ -10,10 +10,12 @@ export class ThreeScene extends React.Component {
   private camera: PerspectiveCamera | null = null;
   private renderer: WebGLRenderer | null = null;
   private frameId: number | null = null;
+  private targetScene: Scene | Group | null = null;
 
   constructor(props: any) {
     super(props);
     this.animate = this.animate.bind(this);
+    this.updatePositionLikeThirdPerson = this.updatePositionLikeThirdPerson.bind(this);
   }
 
   componentDidMount() {}
@@ -25,6 +27,7 @@ export class ThreeScene extends React.Component {
     if (this.scene) {
       this.scene.add(vrm.scene);
     }
+    this.targetScene = vrm.scene;
     return vrm;
   }
 
@@ -35,7 +38,16 @@ export class ThreeScene extends React.Component {
     if (this.scene) {
       this.scene.add(vrm.scene);
     }
+    this.targetScene = vrm.scene;
     return vrm;
+  }
+
+  private updatePositionLikeThirdPerson(){
+    if (this.renderer && this.scene && this.camera && this.targetScene) {
+      const targetPosition = this.targetScene.position;
+      this.camera.position.set(targetPosition.x, targetPosition.y + 1.5, targetPosition.z + 3)
+      this.camera.lookAt(this.targetScene.position)
+    }
   }
 
   private initScene(canvas: HTMLCanvasElement) {
@@ -74,6 +86,7 @@ export class ThreeScene extends React.Component {
     this.frameId = window.requestAnimationFrame(this.animate);
     if (this.renderer && this.scene && this.camera) {
       this.renderer.render(this.scene, this.camera);
+      this.updatePositionLikeThirdPerson();
     }
   }
 
